d558b9373852d0beac61a8895bc8e820cf466437
Hieu Tran <tranduchieu@me.com>
2015-07-07 11:56:33 +0000

    update config

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-var config = require('./config/config.json');
 var app = require('./app');
 var models = require("./models");
 
-app.set('env', config.env || process.env.NODE_ENV || 'development');
-app.set('port', config.port || process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 models.sequelize.sync().then(function () {
   var server = app.listen(app.get('port'), function() {
