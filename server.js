85373b7c121620b5895933907d0d11bb8d3fc68f
Hieu Tran <tranduchieu@me.com>
2015-07-08 12:04:11 +0000

    add config port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config/config.json')[env];
 var app = require('./app');
 var models = require("./models");
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || config.port || 3000);
 
 models.sequelize.sync().then(function () {
   var server = app.listen(app.get('port'), function() {
